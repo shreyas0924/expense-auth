5db70146eae59f88f8a6bcfc068a973e443c0d26
shreyas0924 <shreyasp0924@gmail.com>
2025-02-24 15:58:49 +0530

    updated env and entity scan

--- src/main/java/com/expense/auth/AuthApplication.java
+++ src/main/java/com/expense/auth/AuthApplication.java
@@ -2,15 +2,16 @@ package com.expense.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = { "com.expense.auth.entities" })
 @EnableJpaRepositories(basePackages = { "com.expense.auth.repository" })
 @ComponentScan(basePackages = { "com.expense.auth.auth", "com.expense.auth.controller", "com.expense.auth.service",
 		"com.expense.auth.eventProducer" })
 public class AuthApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
