1d7e1b7f5562146c4a2e1295c5dd4683a6affd8b
shreyas0924 <shreyasp0924@gmail.com>
2025-03-21 12:03:14 +0530

    added loadbyusername

--- src/main/java/com/expense/auth/controller/AuthController.java
+++ src/main/java/com/expense/auth/controller/AuthController.java
@@ -7,6 +7,11 @@ import com.expense.auth.service.JwtService;
 import com.expense.auth.service.RefreshTokenService;
 import com.expense.auth.service.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
-public class AuthController
-{
+public class AuthController {
 
     @Autowired
     private JwtService jwtService;
@@ -32,21 +36,38 @@ public class AuthController
 
     @SuppressWarnings("rawtypes")
     @PostMapping("auth/v1/signup")
-    public ResponseEntity SignUp(@RequestBody UserInfoDto userInfoDto){
-        try{
+    public ResponseEntity SignUp(@RequestBody UserInfoDto userInfoDto) {
+        try {
             Boolean isSignUped = userDetailsService.signupUser(userInfoDto);
-            if(Boolean.FALSE.equals(isSignUped)){
+            if (Boolean.FALSE.equals(isSignUped)) {
                 return new ResponseEntity<>("Already Exist", HttpStatus.BAD_REQUEST);
             }
             RefreshToken refreshToken = refreshTokenService.createRefreshToken(userInfoDto.getUsername());
             String jwtToken = jwtService.GenerateToken(userInfoDto.getUsername());
-            return new ResponseEntity<>(JwtResponseDto.builder().accessToken(jwtToken).
-                    token(refreshToken.getToken()).build(), HttpStatus.OK);
-        }catch (Exception ex){
+            return new ResponseEntity<>(
+                    JwtResponseDto.builder().accessToken(jwtToken).token(refreshToken.getToken()).build(),
+                    HttpStatus.OK);
+        } catch (Exception ex) {
             return new ResponseEntity<>("Exception in User Service", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+    @GetMapping("/auth/v1/ping")
+    public ResponseEntity<String> ping() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            
+            String userId = userDetailsService.getUserByUsername(authentication.getName());
+            if (Objects.nonNull(userId)) {
+                return ResponseEntity.ok(userId);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<Boolean> checkHealth() {
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+
 }
-
-
--- src/main/java/com/expense/auth/service/UserDetailsServiceImpl.java
+++ src/main/java/com/expense/auth/service/UserDetailsServiceImpl.java
@@ -1,6 +1,5 @@
 package com.expense.auth.service;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,14 +20,14 @@ import com.expense.auth.repository.UserRepository;
 
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 @Component
 @AllArgsConstructor
 @Data
-public class UserDetailsServiceImpl implements UserDetailsService
-{
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private final UserRepository userRepository;
@@ -42,12 +41,11 @@ public class UserDetailsServiceImpl implements UserDetailsService
     private static final Logger log = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
-    {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         log.debug("Entering in loadUserByUsername Method...");
         UserInfo user = userRepository.findByUsername(username);
-        if(user == null){
+        if (user == null) {
             log.error("Username not found: " + username);
             throw new UsernameNotFoundException("could not found user..!!");
         }
@@ -55,25 +53,31 @@ public class UserDetailsServiceImpl implements UserDetailsService
         return new CustomUserDetails(user);
     }
 
-    public UserInfo checkIfUserAlreadyExist(UserInfoDto userInfoDto){
+    public UserInfo checkIfUserAlreadyExist(UserInfoDto userInfoDto) {
         return userRepository.findByUsername(userInfoDto.getUsername());
     }
 
-    public Boolean signupUser(UserInfoDto userInfoDto) throws InterruptedException, ExecutionException{
-        //        ValidationUtil.validateUserAttributes(userInfoDto);
+    public String getUserByUsername(String userName) {
+        return Optional.of(userRepository.findByUsername(userName)).map(UserInfo::getUserId).orElse(null);
+    }
+
+    public Boolean signupUser(UserInfoDto userInfoDto) throws InterruptedException, ExecutionException {
+        // ValidationUtil.validateUserAttributes(userInfoDto);
         userInfoDto.setPassword(passwordEncoder.encode(userInfoDto.getPassword()));
-        if(Objects.nonNull(checkIfUserAlreadyExist(userInfoDto))){
+        if (Objects.nonNull(checkIfUserAlreadyExist(userInfoDto))) {
             return false;
         }
         String userId = UUID.randomUUID().toString();
-        userRepository.save(new UserInfo(userId, userInfoDto.getUsername(), userInfoDto.getPassword(), new HashSet<>()));
+        userRepository
+                .save(new UserInfo(userId, userInfoDto.getUsername(), userInfoDto.getPassword(), new HashSet<>()));
         // pushEventToQueue
 
         userInfoProducer.sendEventToKafka(userInfoEventToPublish(userInfoDto, userId));
 
         return true;
     }
-        private UserInfoEvent userInfoEventToPublish(UserInfoDto userInfoDto, String userId){
+
+    private UserInfoEvent userInfoEventToPublish(UserInfoDto userInfoDto, String userId) {
         return UserInfoEvent.builder()
                 .userId(userId)
                 .firstName(userInfoDto.getUsername())
